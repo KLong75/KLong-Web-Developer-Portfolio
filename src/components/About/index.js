bb2a17f93bdb17bffcba582a6833926762149e19
Kevin Long <98487770+KLong75@users.noreply.github.com>
2023-02-05 10:54:13 -0600

    update resume

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -26,18 +26,12 @@ const About = () => {
               <div className='card-body' id='about-me-card-body'>
                 <p 
                   id='about-me-text'>
-                  My path to coding has been conventional only in its unconventionality. I have a Bachelor of Arts in Theater and have worked as a theater artist specializing in puppetry for over 20 years. A life in puppetry has been a wonderful adventure full of interesting people and exciting projects. I would not trade it for anything. 
-
-                  Making a living as an artist is a challenge. As one gets older priorities change. A desire for more stable employment led me to become a certified elementary school teacher. Teaching virtually due to the pandemic deepened my interest in computers and technology. I became curious about how all of the internet tools we were using actually worked and decided to find out.  
-
-                  I graduated from the full stack web development bootcamp at The University of Texas in July of 2022. 
+                  My path to coding has been conventional only in its unconventionality. I have a Bachelor of Arts in Theater and worked as a theater artist specializing in puppetry for many years.
+                  Making a living as an artist is a challenge. As one gets older priorities change. A desire for more stable employment led me to become a certified elementary school teacher. Teaching virtually due to the pandemic sparked an interest in computers and technology. I became curious about how all of the internet tools I was using to teach actually worked and decided to find out.   
                   <hr></hr>
-                  Like coding, puppetry is a highly collaborative, creative process that requires creative problem solving to reach a common goal. Both require a focused group of people working together to build something within a budget by a deadline. The tools and materials are just different.
+                  Like puppetry, coding is a highly collaborative, creative process that requires creative problem solving to reach a common goal. Both require a focused group of people working together to build something within a budget by a deadline. The tools and materials are just different.
                   <hr></hr>
-                  I thrive on creative collaboration and problem solving. The challenge of bringing an idea to life lights me up like nothing else. I love to make things and have fun doing so.
-
-                  I am currently interested in full time employment, freelance gigs, and other collaborative opportunities. Let's build something great.
-
+                  I love to make things. I thrive on creative collaboration and problem solving. I am interested in full time employment, freelance gigs, and other collaborative opportunities. Let's build something great.
                 </p>
               </div>
             </div>
